531cb248e50ad882d36824ce1a56942808025ee6
Cerdic <cedric@yterium.com>
2015-06-30 21:14:55 +0200

    Un formulaire pour configurer les infos de facturation, et top/pied par defaut pour les factures

--- lang/factures_fr.php
+++ lang/factures_fr.php
@@ -8,6 +8,17 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_1_facture' => '1 facture',
 	'info_nb_factures' => '@nb@ factures',
 	'icone_factures' => 'Factures',
+	'label_raison_sociale' => 'Raison sociale',
+	'label_capital' => 'Capital',
+	'label_rcs' => 'N° RCS',
+	'label_siret' => 'N° SIRET',
+	'label_adresse' => 'Adresse',
+	'label_code_postal' => 'Code Postal',
+	'label_ville' => 'Ville',
+	'label_pays' => 'Pays',
+	'label_telephone' => 'Téléphone',
+	'label_email' => 'Email',
+	'au_capital_de' => 'au capital de',
 
 );
 
